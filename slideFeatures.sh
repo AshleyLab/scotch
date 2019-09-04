5464b949794ae4744684f12460948db83a53cefc
charliecurnin <charliecurnin@me.com>
2019-09-03 21:39:46 -0700

    important: slide for N2

--- slideFeatures.sh
+++ slideFeatures.sh
@@ -3,11 +3,11 @@
 fm="$1"
 out="$2"
 
-toSlide="14,15,16,17,18,19,20,21,22,23"
+toSlide="15,16,17,18,19,20,21,22,23,24"
 nToSlide="10"
 useToFill="0"
 delimiter="\t"
 
 fillLine=$(yes "$useToFill" | head -n "$nToSlide" | tr '\n' "$delimiter" | sed "s|$delimiter\$||")
 
-paste "$fm" <(cut -f"$toSlide" "$fm" | tail -n +2; echo "$fillLine") | gzip > "$out"
+paste "$fm" <(cut -f"$toSlide" "$fm" | tail -n +2; echo "$fillLine") > "$out"
